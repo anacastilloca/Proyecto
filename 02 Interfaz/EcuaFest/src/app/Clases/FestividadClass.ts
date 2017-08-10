4fc07ebbc4a7a2b8223e00119cd4cfd29e1fa757
annyCris <ana.castillo@epn.edu.ec>
2017-08-10 08:08:48 -0500

    CSS

---
+++ 02 Interfaz/EcuaFest/src/app/Clases/FestividadClass.ts
@@ -0,0 +1,10 @@
+export class FestividadClass{
+  constructor(public nombreFestividad?:string,
+              public provinciaLugarFestividad?:string,
+              public mesFestividad?:string,
+              public imagenLugarFestividad?:string,
+              public id?:number,
+              public createdAt?:Date,
+              public updatedAt?:Date,
+              public editar?:boolean){}
+}
--- 02 Interfaz/EcuaFest/src/app/Componentes/festividad-inicio/festividad-inicio.component.ts
+++ 02 Interfaz/EcuaFest/src/app/Componentes/festividad-inicio/festividad-inicio.component.ts
@@ -9,13 +9,15 @@ export class FestividadInicioComponent implements OnInit {
 
   //Metodos para el llamdo de las imagenes
    // Encabezado
-      banderaEcuador:string="assets/Imagenes/BanderaEcuador.png";
+      banderaEcuador:string="assets/Imagenes/EcuadorFlag.gif";
+
    //Carrusel
       img1:string="assets/Imagenes/Carrusel/Img1.jpg";
       img2:string="assets/Imagenes/Carrusel/Img2.jpg";
       img3:string="assets/Imagenes/Carrusel/Img3.jpg";
       img4:string="assets/Imagenes/Carrusel/Img4.jpg";
 
+  // LLamado a las Clase Festividad
 
   constructor() { }
 
